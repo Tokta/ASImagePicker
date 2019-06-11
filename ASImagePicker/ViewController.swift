f3ff5d67e1ea7cf2f76b548b304d97800ddaebf4
Alessio <alessio@lepaya.com>
2019-06-11 17:09:47 +0200

    Initial Commit

---
+++ ASImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASImagePicker
+//
+//  Created by Alessio Sardella on 11/06/2019.
+//  Copyright © 2019 Alessio Sardella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
